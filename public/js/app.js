fff233e29ffa353c96298c9052dc1b24a24e67cb
Maiga <maiga93@yahoo.fr>
2016-10-16 01:46:05 +0200

    Game with movement and score management

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,45 @@
 /**
  * Created by qfdk on 16/3/2.
  */
+
 $('#myModal').modal('show')
 
 var socket = io.connect('http://localhost:3000');
 
 $('#btnStart').click(function() {
-    socket.emit('start', { pseudo:$('#pseudo').val()});
+    socket.emit('start', { pseudo: $('#pseudo').val() });
 });
 
 socket.on('ok', function(data) {
-    console.log(data);
+    var score = ""
+    var joueurs = [];
+    joueurs = JSON.parse(data).joueurs;
+
+
+    for (var i = 0; i < joueurs.length; i++) {
+        var j = joueurs[i];
+        score += j.pseudo + " => " + j.score + "</BR> "
+    }
+
+    drawGame(JSON.parse(data));
+    $('#score').html(score)
+});
+
+socket.on('endGame', function(data) {
+    drawGame(JSON.parse(data));
+
+    var result = "Game finished and SCORE are : \n";
+    joueurs = JSON.parse(data).joueurs;
+    for (var i = 0; i < joueurs.length; i++) {
+        var j = joueurs[i];
+        result += j.pseudo + " ===> " + j.score + "\n"
+    }
+    alert(result);
+    location.reload();
+
 });
 
 
-//***********MAIN.js**********//
-var gameBoard;
-var snake;
-var moveDirection = 'right';
-var gameExecutor;
-var gameSpeed=100;
-var roundNum = 1;
-
-var eatenItemsCount =0;
-var MAX_FOOD_ITEMS = 12;
-
-//actual field size(400px) divided by corresponding bodypart size(8px)
-var gameFieldRelativeWidth = 50;
-var gameFieldRelativeHeight = 50;
-
-//width and height of snake body element
-var snakeElementWidth = 8;
-var snakeElementHeight = 8;
-
-//game keys
 var ESC = 27;
 var SPACE = 32;
 var LEFT_ARROW = 37;
@@ -41,281 +47,67 @@ var UP_ARROW = 38;
 var RIGHT_ARROW = 39;
 var DOWN_ARROW = 40;
 
-var food;
 
 $(document).ready(function() {
     $('body').keydown(keyPressedHandler);
 });
 
-function move() {
-	generateFood();
-	snake.move(moveDirection);
-	
-	if(snake.holdsPosition(food.xPos,food.yPos))
-		eatFood();
-		
-	drawSnake();
-};
 
 function keyPressedHandler(e) {
-	var code = (e.keyCode ? e.keyCode : e.which);
-	
-	switch(code) {
-		case LEFT_ARROW:
-			moveDirection = 'left';
-			break;
-		case UP_ARROW:
-			moveDirection = 'up';
-			break;
-		case RIGHT_ARROW:
-			moveDirection = 'right';
-			break;
-		case DOWN_ARROW:
-			moveDirection = 'down';
-			break;
-		case SPACE:
-			startGame();
-			break;
-		case ESC:
-			endGame();
-			break;
-	}
- }
+    var code = (e.keyCode ? e.keyCode : e.which);
 
-function startGame() {
-	gameBoard = new GameBoard();
-	moveDirection = 'right';
-	eatenItemsCount = 0;
-	roundNum = 1;
-	gameSpeed=100;
-	endGame();
-	gameBoard.clearGameInfo();
-	
-	snake = new Snake(80,80);
-	snake.onCrash(snakeCrashHandler,{xPos:400,yPos:400});
-	drawSnake();
-	gameExecutor = setInterval(move,gameSpeed);
-};
-function endGame() {
-	if(gameExecutor)
-		clearInterval(gameExecutor);
-	
-	gameBoard.clearBoard();
-};
-
-function drawSnake() {
-	gameBoard.removeSnakeBody();
-	
-	//draw the new snake
-	var snakeBody = snake.getBody();
-	
-	for(var i=0; i<snakeBody.length; i++){
-		gameBoard.drawElement('bodypart',snakeBody[i].xPos,snakeBody[i].yPos);
-	}
-};
-
-function generateFood() {
-	if(gameBoard.hasNoCreatedFood()){
-		do{
-			xpos = Math.floor(Math.random() * gameFieldRelativeWidth) * snakeElementWidth;
-			ypos = Math.floor(Math.random() * gameFieldRelativeHeight)* snakeElementHeight;
-		}
-		while(snake.holdsPosition(xpos,ypos));
-		food = {xPos:xpos,yPos:ypos};
-		gameBoard.drawElement('food',xpos,ypos);
-	}
-};
-
-function eatFood() {
-	snake.eatFood();
-	gameBoard.removeSnakeFood();
-	
-	eatenItemsCount++;
-	if(eatenItemsCount >= MAX_FOOD_ITEMS)
-		startNextRound();
-	
-	gameBoard.updateScore(roundNum);
-};
-
-function snakeCrashHandler() {
-	endGame();
-	gameBoard.showLoseMessage();
-};
-
-function startNextRound() {
-	roundNum++;
-	eatenItemsCount = 0;
-	gameBoard.showNextRoundMsg();
-	gameSpeed = Math.floor(gameSpeed * 0.8);
-	clearInterval(gameExecutor);
-	gameExecutor = setInterval(move,gameSpeed);
-};
-//***************************//
-//*********SNAKE.js*********//
-function BodyPart(xpos,ypos,direction) {
-	this.xPos=xpos;
-	this.yPos=ypos;
-	this.direction=direction;;
-};
-
-function Snake(startX,startY) {
-	var moveStep = 8;
-	var bodyParts = [new BodyPart(startX,startY,'right')];
-	var reverseDirections = {'right':'left','left':'right','up':'down','down':'up'};
-	var gameRegion;
-	var onCrashCallback;
-	var self = this;
-	
-	this.eatFood = function() {
-		bodyParts.push(getNewTail());
-	};
-	
-	this.move = function(newDirection) {
-		if(isReverseDirection(newDirection))
-			reverseBodyMove();
-			
-		var newHead = getNewHead(newDirection);
-		
-		if(crash(newHead))
-			onCrashCallback();
-		else{		
-			for(var i = bodyParts.length-1; i>0 ;i--){
-				bodyParts[i] = bodyParts[i-1];
-			}
-			bodyParts[0] = newHead;
-		}
-	};
-	
-	this.getBody = function() {
-		return bodyParts;
-	};
-	
-	this.holdsPosition = function(xpos,ypos) {
-		for(var i = 0; i< bodyParts.length; i++){
-			if(bodyParts[i].xPos == xpos && bodyParts[i].yPos == ypos)
-				return true;
-		}
-		return false;
-	};
-	
-	this.onCrash = function(crashCallback,fieldSize) {
-		gameRegion = fieldSize;
-		onCrashCallback = crashCallback;
-	};
-	
-	var getNewHead = function(direction){
-		var currentHead = bodyParts[0];
-		
-		switch(direction){
-			case 'right':
-				return new BodyPart(currentHead.xPos+moveStep,currentHead.yPos,direction);
-			case 'left':
-				return new BodyPart(currentHead.xPos-moveStep,currentHead.yPos,direction);
-			case 'up':
-				return new BodyPart(currentHead.xPos,currentHead.yPos-moveStep,direction);
-			case 'down':
-				return new BodyPart(currentHead.xPos,currentHead.yPos+moveStep,direction);
-		};
-	};
-	
-	var getNewTail = function(){
-		var currentTail = bodyParts[bodyParts.length-1];
-		var tailDirection = currentTail.direction;
-		
-		switch(tailDirection){
-			case 'right':
-				return new BodyPart(currentTail.xPos-moveStep,currentTail.yPos,tailDirection);
-			case 'left':
-				return new BodyPart(currentTail.xPos+moveStep,currentTail.yPos,tailDirection);
-			case 'up':
-				return new BodyPart(currentTail.xPos,currentTail.yPos+moveStep,tailDirection);
-			case 'down':
-				return new BodyPart(currentTail.xPos,currentTail.yPos-moveStep,tailDirection);
-		};
-	};
-	
-	var crash = function(head){
-		if(head.xPos >= gameRegion.xPos
-			|| head.yPos >= gameRegion.yPos
-			|| head.xPos < 0
-			|| head.yPos < 0
-			|| self.holdsPosition(head.xPos,head.yPos))
-			return true;
-		
-		return false;
-	};
-	
-	var isReverseDirection = function(newDirection) {
-		var currentHeadDirection = bodyParts[0].direction;
-		return newDirection == reverseDirections[currentHeadDirection];
-	};
-	
-	var reverseBodyMove = function() {
-		var tmpBodyPart;
-		var halfBodyLength = Math.floor(bodyParts.length/2);
-		var bodyLength = bodyParts.length -1;
-		
-		for(var i = 0; i< halfBodyLength; i++){
-			tmpBodyPart = bodyParts[i];
-			bodyParts[i] = bodyParts[bodyLength - i];
-			bodyParts[bodyLength - i] = tmpBodyPart;
-			bodyParts[i].direction = reverseDirections[bodyParts[i].direction];
-			bodyParts[bodyLength - i].direction = reverseDirections[bodyParts[bodyLength - i]];
-		}
-	};
-};
-//**************************//
-//*******GAMEBOARD.js******//
-function GameBoard() {
-
-	this.drawElement = function (classname, xpos,ypos) {
-		var $element = $('<div/>').addClass(classname);
-		$element.css('top',ypos+'px').css('left',xpos+'px');
-		$('#gameField').append($element);
-	};
-	
-	this.clearBoard = function(){
-		$('div.bodypart').remove();
-		$('.food').remove();
-	};
-	
-	this.clearGameInfo = function() {
-		$('#score').html('0');
-		$('#loseMsg').css('visibility','hidden');
-		$('#speed').html('1');
-	};
-	
-	this.hasNoCreatedFood = function() {
-		return $('.food').length == 0 ;
-	};
-	
-	this.removeSnakeBody = function() {
-		$('div.bodypart').remove();
-	};
-	
-	this.removeSnakeFood = function() {
-		$('.food').remove();
-	};
-	
-	this.updateScore = function(currentRound) {
-		var $currentScore = Number($('#score').html());
-		$currentScore+=currentRound;
-		$('#score').html($currentScore);
-	};
-	
-	this.showLoseMessage = function(){
-		$('#loseMsg').css('visibility','visible');
-	};
-	
-	this.showNextRoundMsg = function() {
-		$('#nextRndMsg').hide().css({visibility: 'visible'}).fadeIn(2000);
-		$('#nextRndMsg').fadeOut(2000, function() {
-				$(this).show().css({visibility: 'hidden'});
-			});
-			
-		var $currentSpeed = Number($('#speed').html());
-		$currentSpeed++;
-		$('#speed').html($currentSpeed);
-	};
+    switch (code) {
+        case LEFT_ARROW:
+        case UP_ARROW:
+        case RIGHT_ARROW:
+        case DOWN_ARROW:
+            socket.emit('move', { direction: code })
+            break;
+        case SPACE:
+            // TODO
+            break;
+        case ESC:
+            // TODO
+            socket.disconnect();
+            location.reload();
+            break;
+        default:
+    }
 }
+
+function drawGame(data) {
+    var c = document.getElementById("gameCanvas");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, c.width, c.height);
+
+    // draw players
+    var joueurs = data.joueurs
+    for (var i = 0; i < joueurs.length; i++) {
+
+        var x = joueurs[i].x;
+        var y = joueurs[i].y;
+        ctx.beginPath();
+        ctx.fillStyle = "black"
+        ctx.fillRect(x, y, 10, 10);
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    // draw food
+    var bonbons = data.bonbons;
+    for (var i = 0; i < bonbons.length; i++) {
+
+        var x = bonbons[i].x;
+        var y = bonbons[i].y;
+
+        ctx.beginPath();
+        ctx.fillStyle = "red"
+        ctx.fillRect(x, y, 10, 10);
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
 //************************//
--- server.js
+++ server.js
@@ -12,9 +12,16 @@ var sockets = []; // Liste des sockets client
 var joueurs = []; // Players avec leurs infos : pseudo, score, position
 
 var bonbons = []; // Liste des Bonbons avec position
-var nbBonbon = 10; // Nombre de bonbon dans le jeune
-var hauteurGrille = 20;
-var largeurGrille = 20;
+var nbBonbon = 2; // Nombre de bonbon dans le jeune
+var sizeOfElement = 10;
+var hauteurGrille = 400 / sizeOfElement;
+var largeurGrille = 400 / sizeOfElement;
+
+// Directions
+var LEFT_ARROW = 37;
+var UP_ARROW = 38;
+var RIGHT_ARROW = 39;
+var DOWN_ARROW = 40;
 
 // ------------express-------------------
 app.set('views', path.join(__dirname, 'views'));
@@ -25,18 +32,18 @@ server.listen(3000);
 console.log('Server on 3000; http://localhost:3000')
 
 // -----------------index-----------------
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get('/api/info', function (req, res) {
+app.get('/api/info', function(req, res) {
     res.render({ user: 'info' });
 });
 
 // ------------------------------------------------
 // -----------------socket.io----------------------
 // ------------------------------------------------
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function(socket) {
 
     //--------------ajouter le client--------------
     if (sockets.indexOf(socket) === -1) {
@@ -44,10 +51,10 @@ io.sockets.on('connection', function (socket) {
         console.log("+1")
     }
 
-    socket.on('start', function (data) {
+    socket.on('start', function(data) {
         var newPosition = generateNewPosition();
-        newX = newPosition % largeurGrille;
-        newY = Math.floor(newPosition / largeurGrille);
+        newX = (newPosition % largeurGrille) * sizeOfElement;
+        newY = (Math.floor(newPosition / largeurGrille)) * sizeOfElement;
         var joueur = {
             score: 0,
             pseudo: data.pseudo,
@@ -66,7 +73,7 @@ io.sockets.on('connection', function (socket) {
     });
 
     // ------------supprimer le client----------
-    socket.on('disconnect', function (o) {
+    socket.on('disconnect', function(o) {
         var index = sockets.indexOf(socket);
         var indexJoueur = getJoueurIndex(socket);
         if (index !== -1) {
@@ -74,6 +81,56 @@ io.sockets.on('connection', function (socket) {
             sockets.splice(index, 1);
             joueurs.splice(indexJoueur, 1);
             console.log("[INFO] " + "Player [" + tmp + "] disconnected.");
+        }
+    });
+
+    // ------------ Move management ----------
+    socket.on('move', function(data) {
+        index = getJoueurIndex(socket);
+
+        var newX, newY;
+        switch (data.direction) {
+            case LEFT_ARROW:
+                newX = joueurs[index].x - sizeOfElement;
+                newY = joueurs[index].y;
+                if (newX >= 0)
+                    moveTo(index, newX, newY);
+                break;
+            case UP_ARROW:
+                newX = joueurs[index].x;
+                newY = joueurs[index].y - sizeOfElement;
+                if (newY >= 0)
+                    moveTo(index, newX, newY);
+                break;
+
+            case RIGHT_ARROW:
+                newX = joueurs[index].x + sizeOfElement;
+                newY = joueurs[index].y;
+                if (newX < largeurGrille * sizeOfElement)
+                    moveTo(index, newX, newY);
+                break;
+            case DOWN_ARROW:
+                newX = joueurs[index].x;
+                newY = joueurs[index].y + sizeOfElement;
+                if (newY < hauteurGrille * sizeOfElement)
+                    moveTo(index, newX, newY);
+                break;
+            default:
+        }
+
+        var result = {
+            "msg": 'ok',
+            "joueurs": joueurs,
+            "bonbons": bonbons
+        };
+
+        if (bonbons.length == 0) {
+            console.log("Bonbon finished")
+            io.sockets.emit('endGame', JSON.stringify(result));
+        }
+        else
+        {
+        io.sockets.emit('ok', JSON.stringify(result));
         }
     });
 });
@@ -88,9 +145,9 @@ for (var i = 0; i < hauteurGrille * largeurGrille; i++) {
 }
 
 /**
-* Generate new position in the game
-* without duplication
-*/
+ * Generate new position in the game
+ * without duplication
+ */
 function generateNewPosition() {
     var number;
     do {
@@ -103,8 +160,8 @@ function generateNewPosition() {
 // Initialisation of the BONBONS
 while (bonbons.length < nbBonbon) {
     var newPosition = generateNewPosition();
-    x = newPosition % largeurGrille;
-    y = Math.floor(newPosition / largeurGrille);
+    x = (newPosition % largeurGrille) * sizeOfElement;
+    y = (Math.floor(newPosition / largeurGrille)) * sizeOfElement;
     bonbons.push({ "x": x, "y": y });
 }
 /**
@@ -114,10 +171,48 @@ while (bonbons.length < nbBonbon) {
 function getJoueurIndex(socket) {
     var id = socket.id;
     var index = -1;
-    joueurs.forEach(function (j) {
+    joueurs.forEach(function(j) {
         if (j.id === id) {
             index = joueurs.indexOf(j);
         }
     }, this);
     return index;
+}
+
+/**
+ * Core of move into new place
+ * 3 cases : new place is Free, there is a player, there is a bonbon
+ */
+function moveTo(indexJoueur, newX, newY) {
+
+    var exit = false;
+
+    // Check if there is already a player in x,y
+    joueurs.forEach(function(j) {
+
+        if (j.x === newX && j.y === newY) {
+            exit = true;
+        }
+    }, this);
+
+
+    // Check if there is a bonbon in x,y
+    if (!exit) {
+        bonbons.forEach(function(b) {
+            if (b.x === newX && b.y === newY) {
+                bonbons.splice(bonbons.indexOf(b), 1);
+
+                joueurs[indexJoueur].x = newX;
+                joueurs[indexJoueur].y = newY;
+                joueurs[indexJoueur].score++;
+
+                exit = true;
+            }
+        }, this);
+    }
+    // The place is Free
+    if (!exit) {
+        joueurs[indexJoueur].x = newX;
+        joueurs[indexJoueur].y = newY;
+    }
 }
