004dd2ed02b15cd9e17e2730dc59d05c981d07ed
ymaiga <maiga93@yahoo.fr>
2016-12-06 23:51:49 +0100

    simulate socket client

--- test2.js
+++ test2.js
@@ -1,38 +1,59 @@
 var assert = require('assert');
-const config = require('./config.js');
-var server = require('./server.js')
+
 var io = require('socket.io-client');
 
-var existedElements = config.existedElements;
-var nbBonbon = config.nbBonbon
-var sizeOfElement = config.sizeOfElement
-var hauteurGrille = config.hauteurGrille
-var largeurGrille = config.largeurGrille
+var socketURL = 'http://localhost:3000';
 
-var bonbons = []; // Liste des Bonbons avec position
 
-describe('[1] Bonbons', function() {
-    describe('1)Bonbons number', function() {
-        it('Bonbons number = initBonbon nb', function() {
-            server.initBonbon(hauteurGrille, largeurGrille, existedElements, bonbons)
-            assert.equal(config.nbBonbon, bonbons.length);
-        });
+var options = {
+    transports: ['websocket'],
+    'force new connection': true
+};
+
+
+/*
+var data1 = { 'pseudo': 'Tom' };
+var data2 = { 'pseudo': 'Sally' };
+
+var client1 = io.connect(socketURL, options);
+
+client1.on('connect', function(data) {
+    client1.emit('start', data1);
+
+    var client2 = io.connect(socketURL, options);
+
+    client2.on('connect', function(data) {
+        client2.emit('start', data2);
     });
-});
 
+});
+*/
+
+var ESC = 27;
+var SPACE = 32;
+var LEFT_ARROW = 37;
+var UP_ARROW = 38;
+var RIGHT_ARROW = 39;
+var DOWN_ARROW = 40;
 
 var clients = []
-var socketURL = "http://localhost:3000"
+var nbClient = 10
 
-for (var i = 0; i < 1000; i++) {
-    clients[i] = io.connect(socketURL);
+for (var i = 0; i < nbClient; i++) {
+    clients[i] = io.connect(socketURL, options)
+
+    var pseudo = 'joueur' + i
+    clients[i].emit('start', { 'pseudo': pseudo });
 }
 
-describe('[2] Socket.io', function() {
-    describe('1)Number client connected', function() {
-        it('client connected', function() {
-            server.initBonbon(hauteurGrille, largeurGrille, existedElements, bonbons)
-            assert.equal(100, server.getJoueurs());
-        });
-    });
-});
+
+setInterval(function() {
+
+    for (var i = 0; i < nbClient; i++) {
+        var randomDirection = Math.floor((Math.random() * 4) + 37);
+        var nbMove = Math.floor((Math.random() * 10));
+        for (var j = 0; j < nbMove; j++) {
+            clients[i].emit('move', { 'direction': randomDirection })
+        }
+    }
+}, 200);
