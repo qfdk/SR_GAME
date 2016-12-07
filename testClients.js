52929de2a0112557fc887ecc269ff4fb5659aa8a
qfdk <qfdk@vip.qq.com>
2016-12-07 15:06:41 +0100

    update testClient

--- testClients.js
+++ testClients.js
@@ -3,32 +3,28 @@ var request = require('request');
 var io = require('socket.io-client');
 var url = "http://localhost:3000/getJoueurs";
 var joueurs = [];
-const NB_joueurs = 20;
+const NB_joueurs = 100;
 
-describe('[1] Joueurs', function () {
-    describe('1)', function () {
-        it('Joueurs number = Our joueurs number ' + NB_joueurs, function () {
-            setTimeout(function () {
-                for (var i = 0; i < NB_joueurs; i++) {
-                    joueurs[i] = io.connect("http://localhost:3000", {
-                        transports: ['websocket'],
-                        'force new connection': true
-                    }).emit('start', { pseudo: "joueur" + i });
-                }
-                request.get({
-                    url: url,
-                    json: true,
-                    headers: { 'User-Agent': 'request' }
-                }, (err, res, data) => {
-                    if (err) {
-                        console.log('Error:', err);
-                    } else if (res.statusCode !== 200) {
-                        console.log('Status:', res.statusCode);
-                    } else {
-                        assert.equal(data.size, NB_joueurs);
-                    }
-                });
-            }, NB_joueurs * 1000);
-        });
-    });
+for (var i = 0; i < NB_joueurs; i++) {
+    joueurs[i] = io.connect("http://localhost:3000", {
+        transports: ['websocket'],
+        'force new connection': false
+    }).emit('start', { pseudo: "joueur" + i });
+}
+
+request.get({
+    url: url,
+    json: true,
+    headers: { 'User-Agent': 'request' }
+}, (err, res, data) => {
+    if (err) {
+        console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+    } else {
+        // console.log(data.size);
+        setTimeout(function () {
+            assert.equal(data.size, NB_joueurs);
+        }, 200000);
+    }
 });
